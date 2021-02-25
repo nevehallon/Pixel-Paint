13721f152ae8f8c2a0edb222287e583ec8ea1851
Neveh Allon <34620850+nevehallon@users.noreply.github.com>
2021-02-25 19:47:18 +0200

    Add: debouce mouseleave events

--- src/common/paintCanvas.tsx
+++ src/common/paintCanvas.tsx
@@ -1,4 +1,16 @@
-import { useEffect, useState } from 'react';
+import { createRef, useEffect, useRef, useState } from 'react';
+
+import { fromEvent } from 'rxjs';
+import {
+  bufferTime,
+  debounceTime,
+  delay,
+  first,
+  last,
+  publishLast,
+  take,
+  takeLast,
+} from 'rxjs/operators/';
 
 import Detector from '../services/detectMouseDown';
 
@@ -16,22 +28,41 @@ export interface CanvasProps {
 
 function PaintCanvas({ fillAction, grid, currentColor }: CanvasProps): any {
   const [gridCopy, setGridCopy] = useState([...grid]);
+  const squareRefs = useRef([]);
+
+  if (squareRefs.current.length !== grid.length) {
+    squareRefs.current = Array(grid.length)
+      .fill('')
+      .map((_, i) => squareRefs.current[i] || createRef());
+  }
+  // prettier-ignore
+  useEffect(() => {
+    const sqrRefs = squareRefs.current.map(({ current }) => current);
+
+     fromEvent(sqrRefs, 'mouseleave').pipe(debounceTime(1000), first()).subscribe(() => fillAction(gridCopy));
+  }, [fillAction, gridCopy]);
 
   /* range 15x15 - 35x35 */
   const sqrt = Math.sqrt(grid.length);
 
-  // prettier-ignore
-  const handleFill = (i: number, e: any | Event): any => {
+  const emitState = (e: any | Event = { type: '', keycode: 0 }): any => {
+    if (e.type === 'keyup' && e.keyCode !== 13) {
+      return;
+    }
     if (!Detector.isMouseDown) {
       // update parent state
       fillAction(gridCopy);
     }
+  };
+
+  // prettier-ignore
+  const handleFill = (i: number, e: any | Event): any => {
     if ((e.type === 'mouseover' && !Detector.isMouseDown) || (e.type === 'keydown' && e.keyCode !== 13)) {
       return;
     }
 
     setGridCopy((gridC) => {
-    const newGrid = [...gridC];
+      const newGrid = [...gridC];
       newGrid[i] = currentColor;
       return newGrid;
     });
@@ -55,7 +86,11 @@ function PaintCanvas({ fillAction, grid, currentColor }: CanvasProps): any {
           className="square"
           key={getCoords(i, arr.length)}
           onKeyDown={(e) => handleFill(i, e)}
+          onKeyUp={(e) => emitState(e)}
+          // onMouseLeave={() => emitState()}
           onMouseOver={(e) => handleFill(i, e)}
+          onMouseUp={() => emitState()}
+          ref={squareRefs.current[i]}
           role="button"
           style={{ backgroundColor: x }}
           tabIndex={0}
